d292f7457e35f59e5a07cac7fa31873562db4014
ddankhazi <ddankhazi@gmail.com>
2025-11-01 15:24:35 +0100

    title change
    title change

--- ddContentBrowser/browser.py
+++ ddContentBrowser/browser.py
@@ -464,7 +464,7 @@ class DDContentBrowser(QtWidgets.QMainWindow):
     
     def setup_ui(self):
         """Setup UI"""
-        self.setWindowTitle("Content Browser for Maya 1.0 by Denes Dankhazi")
+        self.setWindowTitle("Content Browser for Maya | v1.1.1 | by Denes Dankhazi")
         self.setMinimumSize(800, 600)
         
         # Create menu bar
